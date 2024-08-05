3eed8d01f24ecef27c14c67698569b59905aab10
Paolo Dapul <paolodapultech@gmail.com>
2024-08-05 13:27:17 +0800

    feat: implement composition using classes and interfaces

--- ts-training/app/index.tsx
+++ ts-training/app/index.tsx
@@ -1,6 +1,46 @@
 import { Text, View } from "react-native";
 
+interface Vehicle {
+  make: string;
+  model: string;
+  year: number;
+  start(): string;
+}
+
+class Car implements Vehicle {
+  public make: string;
+  public model: string;
+  public year: number;
+  start(): string {
+    return `${this.make} ${this.model} engine is starting...`;
+  }
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+}
+
+class Motorcycle implements Vehicle {
+  public make: string;
+  public model: string;
+  public year: number;
+  start(): string {
+    return `${this.make} ${this.model} is revving up!`;
+  }
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+}
+
 export default function Index() {
+  let myCar = new Car("Toyota", "Corolla", 2020);
+  let myMotorcycle = new Motorcycle("Harley-Davidson", "Street 750", 2019);
+
   return (
     <View
       style={{
@@ -9,7 +49,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World!</Text>
+      <Text>{myCar.start()}</Text>
+      <Text>{myMotorcycle.start()}</Text>
     </View>
   );
 }
